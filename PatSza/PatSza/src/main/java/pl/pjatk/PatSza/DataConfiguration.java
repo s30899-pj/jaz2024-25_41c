cb1d9070eaf0454376e67613b88800dba4387ba2
root <root@DESKTOP-UMC60GQ.>
2025-03-06 00:42:32 +0100

    Done

--- PatSza/PatSza/src/main/java/pl/pjatk/PatSza/DataConfiguration.java
+++ PatSza/PatSza/src/main/java/pl/pjatk/PatSza/DataConfiguration.java
@@ -1,7 +1,9 @@
 package pl.pjatk.PatSza;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
@@ -17,6 +19,12 @@ public class DataConfiguration {
     @Bean
     public List<String> defaultData() {
         return List.of("First String","Second String","Third String","Fourth String", "Fifth String");
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "my.custom", name = "homework", havingValue = "true")
+    public PojoHomework pojoHomework() {
+        return new PojoHomework();
     }
 
 }
---
+++ PatSza/PatSza/src/main/java/pl/pjatk/PatSza/PojoHomework.java
@@ -0,0 +1,8 @@
+package pl.pjatk.PatSza;
+
+public class PojoHomework {
+
+    public PojoHomework() {
+        System.out.println("I'm working");
+    }
+}
