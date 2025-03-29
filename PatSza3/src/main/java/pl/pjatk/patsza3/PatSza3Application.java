678ebe85c621b2a69124c18ae58e94278794d719
s30899 <s30899@pjwstk.edu.pl>
2025-03-29 16:38:55 +0100

    LessonWorkDone

---
+++ PatSza3/src/main/java/pl/pjatk/patsza3/PatSza3Application.java
@@ -0,0 +1,13 @@
+package pl.pjatk.patsza3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatSza3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatSza3Application.class, args);
+    }
+
+}
---
+++ PatSza3/src/main/java/pl/pjatk/patsza3/SandboxController.java
@@ -0,0 +1,38 @@
+package pl.pjatk.patsza3;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/test")
+public class SandboxController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld(@RequestParam Optional<String> reqParam) {
+        return ResponseEntity.ok(reqParam.orElse("Hello World"));
+    }
+
+    @GetMapping("/model")
+    public ResponseEntity<Car> getCar() {
+        Car car = new Car();
+        car.setModel("BMW");
+        car.setColor("red");
+        return ResponseEntity.ok(car);
+    }
+
+    @GetMapping("/hello/{someValue}")
+    public ResponseEntity<String> responseUser(@PathVariable("someValue") String someValue) {
+        return ResponseEntity.ok(someValue);
+    }
+
+    @PostMapping("/model")
+    public ResponseEntity<Car> postCar(@RequestBody Car newCar) {
+        Car car = new Car();
+        car.setModel(newCar.getModel());
+        car.setColor(newCar.getColor());
+        return ResponseEntity.ok(car);
+    }
+
+}
