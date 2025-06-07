82db30c0bbdc367396840e49da7e3db300dcb8e3
s30899 <s30899@pjwstk.edu.pl>
2025-06-07 16:42:56 +0200

    Classwork

--- RentalService/src/main/java/pl/pjatk/RentalService/controller/RentalController.java
+++ RentalService/src/main/java/pl/pjatk/RentalService/controller/RentalController.java
@@ -1,12 +1,26 @@
 package pl.pjatk.RentalService.controller;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.pjatk.RentalService.model.Movie;
 import pl.pjatk.RentalService.service.RentalService;
 
+
 @RestController
 @RequestMapping("/customer")
+@OpenAPIDefinition(info = @Info(
+        title = "Rental Service API Documentation",
+        description = "This is the documentation for the sample Rental service project with REST API"))
+@Tag(name = "Rental Controller", description = "Basic endpoints for customer")
 public class RentalController {
     private final RentalService rentalService;
 
@@ -14,19 +28,36 @@ public class RentalController {
         this.rentalService = rentalService;
     }
 
+    @Operation(summary = "Get a movie by id", description = "Returns a movie as per the id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved", content =
+                    { @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "404", description = "Not found - The movie was not found", content = @Content)
+    })
     @GetMapping("/movie/{id}")
-    public ResponseEntity<Movie> getMovie(@PathVariable Long id) {
+    public ResponseEntity<Movie> getMovie(@PathVariable("id") @Parameter(name = "id", description = "Movie id", example = "1") Long id) {
         return ResponseEntity.ok(rentalService.getMovie(id));
     }
 
+    @Operation(summary = "Change availability of movie on true", description = "Returns none, but changing variable in database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully change availability on false"),
+            @ApiResponse(responseCode = "404", description = "Not found - The movie was not found")
+    })
     @PostMapping("/movie/isAvailable/{id}")
-    public ResponseEntity<Void> returnMovie(@PathVariable Long id) {
+    public ResponseEntity<Void> returnMovie(@PathVariable("id") @Parameter(name = "id", description = "Movie id", example = "1") Long id) {
         rentalService.returnMovie(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Change availability of movie on false", description = "Returns none, but changing variable in database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully change availability on false"),
+            @ApiResponse(responseCode = "404", description = "Not found - The movie was not found")
+    })
     @PostMapping("/movie/isNotAvailable/{id}")
-    public ResponseEntity<Void> rentMovie(@PathVariable Long id) {
+    public ResponseEntity<Void> rentMovie(@PathVariable("id") @Parameter(name = "id", description = "Movie id", example = "1") Long id) {
         rentalService.rentMovie(id);
         return ResponseEntity.noContent().build();
     }
--- RentalService/src/main/java/pl/pjatk/RentalService/model/Movie.java
+++ RentalService/src/main/java/pl/pjatk/RentalService/model/Movie.java
@@ -1,5 +1,6 @@
 package pl.pjatk.RentalService.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +11,17 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class Movie {
+    @Schema(description = "Movie ID", example = "1")
     private Integer id;
+    @Schema(description = "Movie title", example = "Scream")
     private String title;
+    @Schema(description = "Movie category", example = "horror")
     private String category;
+    @Schema(description = "Movie release date", example = "1996-12-18")
     private LocalDate releaseDate;
+    @Schema(description = "Movie duration in minutes", example = "111")
     private Integer duration;
+    @Schema(description = "Movie availability", example = "true", defaultValue = "false")
     private boolean available = false;
 
 }
