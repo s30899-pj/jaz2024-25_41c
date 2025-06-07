7b480df85dcf5b22e0870ea480e6404955a9ec6e
s30899-pj <s30899@pjwstk.edu.pl>
2025-06-07 04:50:03 +0200

    HW Done

--- RentalService/src/main/java/pl/pjatk/RentalService/service/RentalService.java
+++ RentalService/src/main/java/pl/pjatk/RentalService/service/RentalService.java
@@ -1,9 +1,16 @@
 package pl.pjatk.RentalService.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 import pl.pjatk.RentalService.model.Movie;
+
+import java.net.ConnectException;
+import java.util.function.Supplier;
 
 @Service
 public class RentalService {
@@ -15,27 +22,44 @@ public class RentalService {
     }
 
     public Movie getMovie(Long id) {
-        try {
-            return restTemplate.getForObject("http://localhost:8080/cinema/movies/" + id, Movie.class);
-        } catch (RestClientException e) {
-            System.out.println("Error fetching movie from API: " + e.getMessage());
-            return null;
-        }
+        return executeWithHandling(() -> restTemplate.getForObject("http://localhost:8080/cinema/movies/" + id, Movie.class));
     }
 
     public void returnMovie(Long id) {
-        try {
-            restTemplate.postForEntity("http://localhost:8080/cinema/movies/isAvailable/" + id, null, Void.class);
-        } catch (RestClientException e) {
-            System.out.println("Error fetching movie from API: " + e.getMessage());
-        }
+        executeWithHandling(() -> restTemplate.postForEntity("http://localhost:8080/cinema/movies/isAvailable/" + id, null, Void.class));
     }
 
     public void rentMovie(Long id) {
+        executeWithHandling(() -> restTemplate.postForEntity("http://localhost:8080/cinema/movies/isNotAvailable/" + id, null, Void.class));
+    }
+
+    public <T> T executeWithHandling(Supplier<T> supplier) {
         try {
-            restTemplate.postForEntity("http://localhost:8080/cinema/movies/isNotAvailable/" + id, null, Void.class);
-        } catch (RestClientException e) {
-            System.out.println("Error fetching movie from API: " + e.getMessage());
+            return supplier.get();
+        } catch (HttpServerErrorException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_GATEWAY);
+        } catch (HttpClientErrorException e) {
+            throw new ResponseStatusException(e.getStatusCode());
+        } catch (ResourceAccessException e) {
+            if (e.getCause() instanceof ConnectException) {
+                throw new ResponseStatusException(HttpStatus.GATEWAY_TIMEOUT);
+            }
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    public void executeWithHandling(Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (HttpServerErrorException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_GATEWAY);
+        } catch (HttpClientErrorException e) {
+            throw new ResponseStatusException(e.getStatusCode());
+        } catch (ResourceAccessException e) {
+            if (e.getCause() instanceof ConnectException) {
+                throw new ResponseStatusException(HttpStatus.GATEWAY_TIMEOUT);
+            }
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
 }
